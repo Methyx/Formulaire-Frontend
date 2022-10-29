80b05f33bf3cc856ef7784556a88577d70d741eb
Philippe MERCY <philippemercy@macbook-pro-de-philippe.home>
2022-10-29 17:40:41 +0200

    First Commit

---
+++ assets/js/doc-loaded.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded");
+
+  document.querySelector("#form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+      firstname: document.querySelector("#first-name").value,
+      lastname: document.querySelector("#last-name").value,
+      email: document.querySelector("#mail").value,
+      subject: document.querySelector("#subject").value,
+      message: document.querySelector("#message").value,
+    };
+    try {
+      document.getElementById("nok").classList.add("hidden");
+      document.getElementById("ok").classList.add("hidden");
+      const response = await axios.post("http://localhost:3000/form", data);
+      console.log(response.status);
+      if (response.data.message === "reçu") {
+        document.getElementById("ok").classList.remove("hidden");
+        setTimeout(() => {
+          location.reload();
+        }, 1000);
+      } else {
+        document.getElementById("nok").classList.remove("hidden");
+      }
+    } catch (error) {
+      document.getElementById("nok").classList.remove("hidden");
+    }
+  });
+});
